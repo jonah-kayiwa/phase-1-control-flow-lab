e2d05dce2d908bdc4ac84f9371aae6c0be68f733
Jonah <kayiwa.jonah@student.moringaschool.com>
2023-12-21 01:51:40 +0300

    ####labs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This one is on me !"
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+if (destination === Nairobi){
+  return "Sure,I shall take you there!"
+}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip => 50){
+    return "You are the best !"
+  }
 }
