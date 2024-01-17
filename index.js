611eeb0c6fbf7c7bf9c054195d66f09dfea4157a
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-01-17 09:16:34 +0300

    completed

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-function scuberGreetingForFeet(distance){
-  // Write your code here!
-  if (distance <= 400){
-    return "This one is on me !"
+function scuberGreetingForFeet(feet) {
+
+  let response;
+  if (feet <= 400) {
+    response = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    response = 'No can do.';
   }
+  return response;
 }
 
-function ternaryCheckCity(destination){
-  // Write your code here!
-if (destination === Nairobi){
-  return "Sure,I shall take you there!"
-}
+function ternaryCheckCity(city) {
+  //Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-  if (tip => 50){
-    return "You are the best !"
+function switchOnCharmFromTip(tip) {
+  //Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 }
